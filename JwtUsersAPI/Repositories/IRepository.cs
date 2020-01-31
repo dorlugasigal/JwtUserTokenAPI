9d56c1195c5c529efe670f1925e6a26cffcf9c9d
Dor Lugasi-Gal <dorlugasigal@gmail.comgit>
2020-01-31 23:01:03 +0200

    :zap: complete User Repository class, with exception handling

--- JwtUsersAPI/Repositories/IRepository.cs
+++ JwtUsersAPI/Repositories/IRepository.cs
@@ -9,7 +9,7 @@ namespace JwtUsersAPI.Repositories
         Task<List<T>> GetAll();
         Task<T> Get(int id);
         Task<T> Add(T entity);
-        Task<T> Update(T entity);
-        Task<T> Delete(int id);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(int id);
     }
 }
--- JwtUsersAPI/Repositories/UsersRepository.cs
+++ JwtUsersAPI/Repositories/UsersRepository.cs
@@ -4,42 +4,101 @@ using System.Threading.Tasks;
 using JwtUsersAPI.Data;
 using JwtUsersAPI.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace JwtUsersAPI.Repositories
 {
     public class UsersRepository : IRepository<User>
     {
         private readonly UsersContext _context;
+        private readonly ILogger _logger;
 
-        public UsersRepository(UsersContext context)
+        public UsersRepository(UsersContext context, ILogger logger)
         {
             _context = context;
+            _logger = logger;
         }
+
         public async Task<List<User>> GetAll()
         {
-            return await _context.Users.ToListAsync();
+            try
+            {
+                return await _context.Users.ToListAsync();
+            }
+            catch (System.Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, ex.Message);
+                return null;
+            }
         }
 
         public async Task<User> Get(int id)
         {
-            return await _context.Users.FindAsync(id);
+            try
+            {
+                return await _context.Users.FindAsync(id);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, ex.Message);
+                return null;
+            }
         }
 
         public async Task<User> Add(User entity)
         {
-            var user = await _context.Users.AddAsync(entity);
-            await _context.SaveChangesAsync();
-            return user.Entity;
+            try
+            {
+                var user = await _context.Users.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                return user.Entity;
+            }
+            catch (System.Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, ex.Message);
+                return null;
+            }
         }
 
-        public async Task<User> Update(User entity)
+        public async Task<bool> Update(User entity)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var user = await _context.Users.FindAsync(entity.Id);
+                if (user == null)
+                {
+                    return false;
+                }
+                _context.Entry(entity).CurrentValues.SetValues(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, ex.Message);
+                return false;
+            }
         }
 
-        public async Task<User> Delete(int id)
+        public async Task<bool> Delete(int id)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var user = await _context.Users.FindAsync(id);
+                if (user == null)
+                {
+                    return false;
+                }
+
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, ex.Message);
+                return false;
+            }
         }
     }
 }
