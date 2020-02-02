fecac0937da140af015f242422b5d3403c77242a
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2020-02-02 16:34:26 +0200

    :art: refactoring

--- JwtUsersAPI/Controllers/UsersControllerV2.cs
+++ JwtUsersAPI/Controllers/UsersControllerV2.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using JwtUsersAPI.Entities;
+﻿using JwtUsersAPI.Entities;
 using JwtUsersAPI.Models;
 using JwtUsersAPI.Services;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace JwtUsersAPI.Controllers
 {
